33ad22029f4a781b355e701c62993b41e659d5b1
cooljasonmelton <cooljasonmelton>
2020-03-06 10:42:19 -0600

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+
+
+def collect_multiples(limit)     
+    num_array = (1...limit).to_a
+    num_array.select do |n|
+        n % 3 == 0 || n % 5 == 0
+    end 
+end 
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end 
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,22 @@
 # Enter your object-oriented solution here!
+require 'pry'
+
+
+class Multiples
+    attr_accessor :limit
+
+    def initialize(limit)
+        @limit = limit
+    end 
+
+    def collect_multiples     
+        num_array = (1...self.limit).to_a
+        num_array.select do |n|
+            n % 3 == 0 || n % 5 == 0
+        end 
+    end 
+    
+    def sum_multiples
+        collect_multiples.sum
+    end 
+end 
